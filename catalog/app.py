afc8c46153bd5fa030a21cd56dbdacaf8f141f00
ShamsAziz <s12113658@stu.najah.edu>
2025-04-02 20:37:38 +0300

    Fix formatting in app.py for consistency

--- catalog/app.py
+++ catalog/app.py
@@ -69,5 +69,5 @@ def query_by_item(item_id):
             })
     return jsonify({'error': 'Book not found'}), 404
 
- if __name__ == '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
