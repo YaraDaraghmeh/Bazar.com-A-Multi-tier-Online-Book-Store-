0e81d2ce0aff215ba0bf6eddc4fd3fb031e49656
YaraDaraghmeh <yaradaraghmeh056@gmail.com>
2025-04-02 21:18:26 +0300

    Add search API endpoint to retrieve catalog items by topic

--- frontend/app.py
+++ frontend/app.py
@@ -10,3 +10,8 @@ ORDER_SERVICE_URL = "http://order:5001"
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/search/<topic>', methods=['GET'])
+def search(topic):
+    response = requests.get(f"{CATALOG_SERVICE_URL}/search/{topic}")
+    return jsonify(response.json())
