a982e215588daff885ae736f444120ba63712ab9
ShamsAziz <s12113658@stu.najah.edu>
2025-04-02 21:18:19 +0300

    Add API endpoint to retrieve item information from catalog service

--- frontend/app.py
+++ frontend/app.py
@@ -10,3 +10,10 @@ ORDER_SERVICE_URL = "http://order:5001"
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+
+@app.route('/api/info/<int:item_id>', methods=['GET'])
+def info(item_id):
+    response = requests.get(f"{CATALOG_SERVICE_URL}/info/{item_id}")
+    return jsonify(response.json())
