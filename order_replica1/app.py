eed8186b367fcc32e3368dd1260d8c3c827540b8
YaraDaraghmeh <yaradaraghmeh056@gmail.com>
2025-05-28 22:07:04 +0300

    Update app.py to define replica URLs and catalog replicas; change server port to 5005

--- order_replica1/app.py
+++ order_replica1/app.py
@@ -7,7 +7,21 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+replica_urls = [
+    "http://order:5001",
+    "http://order_replica2:5006"
+]
 
+catalog_index = 0
+catalog_replicas = [
+    "http://catalog:5000",
+    "http://catalog_replica1:5003",
+    "http://catalog_replica2:5004"
+]
+
+
+DATA_FILE = "data/orders.csv"
+os.makedirs(os.path.dirname(DATA_FILE), exist_ok=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5005)
